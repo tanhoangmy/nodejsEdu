80bad769d3ca1c1c72025a144d9b5a02847e1564
tanhoangmy <tanhoangmy.giamy@gmail.com>
2022-08-23 15:42:30 +0700

    update url module

---
+++ url_module.js
@@ -0,0 +1,13 @@
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+
+var q = url.parse(adr, true);
+
+console.log(q);
+console.log(q.host);
+console.log(q.port);
+console.log(q.pathname);
+console.log(q.search);
+
+var qdata = q.query;
+console.log(qdata.month);
